c2ae8ef19f21bea021c3de35b55a8b015245780a
greenteamer <greenteamer@bk.ru>
2015-08-15 07:38:01 +0600

    media in settings local

--- project/settings.py
+++ project/settings.py
@@ -81,7 +81,7 @@ USE_L10N = True
 # Example: "/home/media/media.lawrence.com/media/"
 # MEDIA_ROOT = 'C:/webmagazinedjango/webshop/static/media/'
 # MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media').replace('\\', '/')
-MEDIA_ROOT = '%s/project/media' % CURRPATH
+MEDIA_ROOT = ''
 CKEDITOR_UPLOAD_PATH = '/media/uploads'
 DIRECTORY = os.path.join(CURRPATH, 'media/uploads')
 
