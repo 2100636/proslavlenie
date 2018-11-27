56499be5e965b8e152b650e16e8372f7d2d07326
alexey1590 <alexey1590@mail.ru>
2018-11-27 19:35:32 +0700

    //

--- project/core/views.py
+++ project/core/views.py
@@ -397,7 +397,7 @@ def advertAllView(request, template_name="catalog/advert_all.html"):
 
 
     adverts = Advert.objects.filter(status=1).order_by("-id")
-    paginator = Paginator(adverts, 2)
+    paginator = Paginator(adverts, 5)
     pageNumber = request.GET.get('page')
 
     try: 
